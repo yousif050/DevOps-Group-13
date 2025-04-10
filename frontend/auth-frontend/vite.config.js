e2257c5e862dfca4420795544a92e482515de253
Yanhua He <yhe494@gmail.com>
2025-04-09 22:14:10 -0400

    Add User Auth Frontend (Closes #2)

--- frontend/auth-frontend/vite.config.js
+++ frontend/auth-frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// user-app/src/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'userApp',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+      shared: ['react', 'react-dom', '@apollo/client', 'graphql'],
+    }),
+  ],
+
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
